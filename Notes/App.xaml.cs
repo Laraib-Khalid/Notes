946f7bea013b0d43c40f46a1c6ae8ea829f6fd8d
Laraib Khaild <laraibkhalid7861@gmail.com>
2021-04-13 20:43:34 +0500

    declare folder path in app.xaml.cs , make note entry page ,changes in notes page , use appearing method

--- Notes/App.xaml.cs
+++ Notes/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Notes.Views;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,10 +8,11 @@ namespace Notes
 {
     public partial class App : Application
     {
-
+        public static string FolderPath { get; private set; }
         public App()
         {
             InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new AppShell();
         }
 
